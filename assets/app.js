1919e081e6fd5969b8b4d05a7e7fbbc8c36304bb
sam <coquelet.samuel@gmail.com>
2023-01-19 11:00:16 +0100

    back recommencement

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,10 @@
 var but = document.getElementById('but');
-var hour = document.getElementById('hours');
-var days = document.getElementById('days');
 var containerTop = document.getElementById('containerTop');
 var tmpl = document.getElementById('tmpl');
-var divTitle = document.getElementsByClassName('divTitle');
-var containTache = document.getElementsByClassName('containTache');
 var addBut = document.getElementsByClassName('new');
-var del = document.getElementById('del');
-var currentTacheA = document.getElementsByClassName('taches');
-;
-var currentTache = 1;
-var currentButton = 1;
 var currentProjet = 1;
-// creation d'un projet ---------------------------------------------------------------
+var currentButton = 1;
+// Create a new project when the button is clicked
 but.addEventListener('click', function () {
     if (tmpl.content) {
         containerTop.append(tmpl.content.cloneNode(true));
@@ -21,29 +13,20 @@ but.addEventListener('click', function () {
             projet.id = 'container' + currentProjet.toString();
             currentProjet++;
         });
-        var buttonAddT_1 = document.querySelectorAll('.new');
-        buttonAddT_1.forEach(function (button) {
-            button.id = 'buttonProject' + currentButton.toString();
-            currentButton++;
-        });
-        var currentTacheB = document.querySelectorAll('.taches');
-        currentTacheB.forEach(function (dtl) {
-            dtl.id = 'tachesList' + currentTache.toString();
-            currentTache++;
-            // creation d'une tache ---------------------------------------------------------
-            function createTache(event) {
-                var button = event.target;
-                var detailListe = document.createElement('div');
-                detailListe.className = 'containTache';
-                detailListe.innerText = "1";
-                button.parentNode.appendChild(detailListe);
-            }
-            for (var i = 0; i < currentTacheA.length; i++) {
-                if (currentTacheA[i]) {
-                    // @ts-ignore
-                    buttonAddT_1[i].addEventListener('click', function (event) { return createTache(event); });
-                }
-            }
-        });
+    }
+});
+// Create a new task when the "add task" button is clicked
+function createTache(event) {
+    var button = event.target;
+    var task = document.createElement('div');
+    task.className = 'task';
+    task.innerText = "New Task";
+    var project = button.closest('.container');
+    project.querySelector('.taches').appendChild(task);
+}
+document.addEventListener('click', function (event) {
+    var button = event.target;
+    if (button.classList.contains('new')) {
+        createTache(event);
     }
 });
--- assets/app.ts
+++ assets/app.ts
@@ -1,20 +1,13 @@
-let but = document.getElementById('but') as HTMLElement;
-let hour = document.getElementById('hours') as HTMLElement;
-let days = document.getElementById('days') as HTMLElement;
+let but = document.getElementById('but') as HTMLButtonElement;
 let containerTop = document.getElementById('containerTop') as HTMLElement;
-let tmpl = document.getElementById('tmpl') as HTMLElement;
-let divTitle = document.getElementsByClassName('divTitle') as HTMLCollectionOf<HTMLElement>;
-let containTache = document.getElementsByClassName('containTache') as HTMLCollectionOf<HTMLElement>;
-let addBut = document.getElementsByClassName('new') as HTMLCollectionOf<HTMLElement>;
-let del = document.getElementById('del') as HTMLElement;
-let currentTacheA = document.getElementsByClassName('taches') as HTMLCollectionOf<HTMLElement>;;
+let tmpl = document.getElementById('tmpl') as HTMLTemplateElement;
+let addBut = document.getElementsByClassName('new') as HTMLCollectionOf<HTMLButtonElement>;
 
-let currentTache: number = 1;
-let currentButton: number = 1;
 let currentProjet: number = 1;
+let currentButton: number = 1;
 
-// creation d'un projet ---------------------------------------------------------------
-but.addEventListener('click', function() {
+// Create a new project when the button is clicked
+but.addEventListener('click', () => {
     if (tmpl.content) {
         containerTop.append(tmpl.content.cloneNode(true));
         let projects = document.querySelectorAll('.container');
@@ -22,34 +15,22 @@ but.addEventListener('click', function() {
             projet.id = 'container' + currentProjet.toString();
             currentProjet++;
         });
+    }
+});
 
-        let buttonAddT = document.querySelectorAll('.new');
-        buttonAddT.forEach((button) => {
-            button.id = 'buttonProject' + currentButton.toString();
-            currentButton++;
-        });
+// Create a new task when the "add task" button is clicked
+function createTache(event: MouseEvent) {
+    let button = event.target as HTMLButtonElement;
+    let task = document.createElement('div');
+    task.className = 'task';
+    task.innerText = "New Task";
+    let project = button.closest('.container');
+    project.querySelector('.taches').appendChild(task);
+}
 
-        let currentTacheB = document.querySelectorAll('.taches')
-        currentTacheB.forEach((dtl) => {
-            dtl.id = 'tachesList' + currentTache.toString();
-            currentTache++;
-
-            // creation d'une tache ---------------------------------------------------------
-
-            function createTache(event: MouseEvent) {
-                let button = event.target as HTMLButtonElement;
-                let detailListe = document.createElement('div');
-                detailListe.className = 'containTache';
-                detailListe.innerText = "1";
-                button.parentNode.appendChild(detailListe);
-            }
-            for (let i = 0; i < currentTacheA.length; i++) {
-                if (currentTacheA[i]) {
-                    // @ts-ignore
-                    buttonAddT[i].addEventListener('click', (event: MouseEvent) => createTache(event));
-                }
-            }
-
-        });
+document.addEventListener('click', (event: MouseEvent) => {
+    let button = event.target as HTMLButtonElement;
+    if(button.classList.contains('new')) {
+        createTache(event);
     }
 });
